07ae63d442b38e631864150f202554a8525fcfe8
cxrloveuu <974616049@qq.com>
2019-08-16 18:31:02 +0800

    fix: 优化JS运行

--- src/preview.ts
+++ src/preview.ts
@@ -1,21 +1,20 @@
 import 'quill/assets/snow.styl'
 import './preview.styl'
-const imgArr = Array.from(document.querySelectorAll('img[data-width]'))
-console.log('这个是图片列表', imgArr)
-imgArr.forEach(element => {
-  const width = +element.getAttribute('data-width')!
-  const height = +element.getAttribute('data-height')!
-  console.log('这里是图片原始宽高', width, height)
-  let newWidth: number = 0
-  let newHeight: number = 0
-  const el = document.querySelector('.ql-editor')!
-  if (width > el.clientWidth) {
-    newWidth = el.clientWidth
-    newHeight = height * (el.clientWidth / width)
-  } else {
-    newWidth = width
-    newHeight = height
-  }
-  console.log(newWidth, newHeight)
-  element.setAttribute('style', `width:${newWidth}px;height:${newHeight}px`)
-})
+setTimeout(() => {
+  const imgArr = Array.from(document.querySelectorAll('img[data-width]'))
+  imgArr.forEach(element => {
+    const width = +element.getAttribute('data-width')!
+    const height = +element.getAttribute('data-height')!
+    let newWidth: number = 0
+    let newHeight: number = 0
+    const el = document.querySelector('.ql-editor')!
+    if (width > el.clientWidth) {
+      newWidth = el.clientWidth
+      newHeight = height * (el.clientWidth / width)
+    } else {
+      newWidth = width
+      newHeight = height
+    }
+    element.setAttribute('style', `width:${newWidth}px;height:${newHeight}px`)
+  })
+}, 10)

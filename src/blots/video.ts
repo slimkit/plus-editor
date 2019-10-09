07e53428da40bbbdd8e09c82e380ada99bd1e35e
xiewme <i@xiew.me>
2019-10-09 16:06:23 +0800

    chore: 阻止默认事件

--- src/blots/video.ts
+++ src/blots/video.ts
@@ -21,6 +21,7 @@ class VideoBlot extends BlockEmbed {
       (e: any) => {
         e.target.paused ? e.target.play() : e.target.pause()
         e.stopPropagation()
+        e.preventDefault()
       },
       false,
     )

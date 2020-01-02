1a860dbf14296d1f0d95014b3013ccc0f7b37dc6
xiewme <i@xiew.me>
2020-01-02 19:24:13 +0800

    feat: 支持设置编辑器ID

--- src/caller.ts
+++ src/caller.ts
@@ -26,32 +26,38 @@ export function callMethod(fnName: string, params: any = undefined) {
   } else if (window.self != window.parent) {
     // in iframe
     if (!tryHijackUploadCall(fnName, params)) {
-      window.parent.postMessage({ funcName: fnName, params }, '*')
+      let id: string | null = null
+      try {
+        id = new URL(window.location.href).searchParams.get('id')
+      } catch (err) {
+        //
+      }
+      window.parent.postMessage({ funcName: fnName, params, id }, '*')
     }
   } else {
     // not in webview
-    tryHijackUploadCall(fnName, params)
+    // tryHijackUploadCall(fnName, params)
     return false
   }
 
   return true
 }
 
-enum FuncName {
-  setUploaderOptions = 'setUploaderOptions',
-  setContentReceiver = 'setContentReceiver',
-  imagePreviewReceiver = 'imagePreviewReceiver',
-  imageProgressReceiver = 'imageProgressReceiver',
-  imageUrlReceiver = 'imageUrlReceiver',
-  imageFailedReceiver = 'imageFailedReceiver',
-  videoPreviewReceiver = 'videoPreviewReceiver',
-  videoProgressReceiver = 'videoProgressReceiver',
-  videoUrlReceiver = 'videoUrlReceiver',
-  videoFailedReceiver = 'videoFailedReceiver',
-  editorSubmitReceiver = 'editorSubmitReceiver',
-  changePlaceholder = 'changePlaceholder',
-  pageHiddenReceiver = 'pageHiddenReceiver',
-}
+type FuncName =
+  | 'setUploaderOptions'
+  | 'setContentReceiver'
+  | 'imagePreviewReceiver'
+  | 'imageProgressReceiver'
+  | 'imageUrlReceiver'
+  | 'imageFailedReceiver'
+  | 'videoPreviewReceiver'
+  | 'videoProgressReceiver'
+  | 'videoUrlReceiver'
+  | 'videoFailedReceiver'
+  | 'editorSubmitReceiver'
+  | 'changePlaceholder'
+  | 'changePlaceholder'
+  | 'pageHiddenReceiver'
 
 const postMessage = window.postMessage
 window.postMessage = function(
